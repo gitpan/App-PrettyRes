fa0be4f55318a1bcb0cf96a8649f9c4db460c8ee
Steven Haryanto <stevenharyanto@gmail.com>
2014-04-22 05:02:28 +0000

    Import of SHARYANTO/App-PrettyRes-0.01 from CPAN.
    gitpan-cpan-distribution: App-PrettyRes
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/App-PrettyRes-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/App/PrettyRes.pm
@@ -0,0 +1,54 @@
+package App::PrettyRes;
+
+our $VERSION = '0.01'; # VERSION
+our $DATE = '2014-04-22'; # DATE
+
+1;
+# ABSTRACT: Format enveloped result prettily
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+App::PrettyRes - Format enveloped result prettily
+
+=head1 VERSION
+
+version 0.01
+
+=head1 SYNOPSIS
+
+See the provided command-line script L<pretty-res>.
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/App-PrettyRes>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/sharyanto/perl-App-PrettyRes>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=App-PrettyRes>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
